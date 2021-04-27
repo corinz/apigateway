26120e3a7950c8bdef9021ad663946fb23881db8
Corin Ziyadeh <corin@boxboat.com>
2021-04-27 16:19:52 -0400

    Corrected startup call

--- cmd/server/api_test.go
+++ cmd/server/api_test.go
@@ -18,7 +18,7 @@ var URL string = "http://localhost:8080"
 
 func TestWebServerRunning(t *testing.T) {
 	app := app.NewApp()
-	go app.Startup(":8080")
+	go app.Startup()
 	defer app.Shutdown()
 	_, err := http.Get("http://localhost:8080")
 	if err != nil {
@@ -28,7 +28,7 @@ func TestWebServerRunning(t *testing.T) {
 
 func TestAPICreate(t *testing.T) {
 	app := app.NewApp()
-	go app.Startup(":8080")
+	go app.Startup()
 	defer app.Shutdown()
 
 	var json = []byte(`{"Name":"testAPI"}`)
@@ -40,7 +40,7 @@ func TestAPICreate(t *testing.T) {
 
 func TestAPIEndpoint(t *testing.T) {
 	app := app.NewApp()
-	go app.Startup(":8080")
+	go app.Startup()
 	defer app.Shutdown()
 
 	// API Create
@@ -60,7 +60,7 @@ func TestAPIEndpoint(t *testing.T) {
 
 func TestBadCreate(t *testing.T) {
 	app := app.NewApp()
-	go app.Startup(":8080")
+	go app.Startup()
 	defer app.Shutdown()
 
 	// Existing API
